9a09225a7a3017dedd90395d79582b118b71043c
Rajvee Paliwal <rajveepaliwal11@gmail.com>
2025-07-23 17:14:05 +0530

    Day1 Springboot- Features

---
+++ springbootfeatures/src/main/java/com/example/springbootfeatures/SpringbootfeaturesApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootfeatures;
+import  org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfeaturesApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootfeaturesApplication.class, args);
+		System.out.println("Application Started");
+	}
+
+}
