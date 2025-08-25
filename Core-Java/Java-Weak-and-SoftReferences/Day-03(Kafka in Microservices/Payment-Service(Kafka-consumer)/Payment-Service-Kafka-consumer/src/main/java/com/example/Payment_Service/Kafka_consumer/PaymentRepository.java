08e382a632a844e5538b5768dc1e6ab35616deac
RajveePaliwal <64592881+r14112000@users.noreply.github.com>
2025-08-25 14:59:06 +0530

    Delete Core-Java/Java-Weak-and-SoftReferences/Day-03(Kafka in Microservices directory

--- Core-Java/Java-Weak-and-SoftReferences/Day-03(Kafka in Microservices/Payment-Service(Kafka-consumer)/Payment-Service-Kafka-consumer/src/main/java/com/example/Payment_Service/Kafka_consumer/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Payment_Service.Kafka_consumer;
-
-import com.example.Payment_Service.Kafka_consumer.Entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository  extends JpaRepository<Payment, String> {
-}
